2f00cf962c6ec8ed1369208cc14e35f333c64624
Iván Montiel cardona <ivan_montiel2013@hotmail.com>
2021-01-19 18:05:49 -0600

    Edit Tema 3

--- src/Tema 3 - Reloj/Hora.java
+++ src/Tema 3 - Reloj/Hora.java
@@ -1,21 +1,30 @@
 
+ /**
+ * Esta clase implementa el objeto hora
+ *
+ * @ autor Iván Montiel Cardona
+ * @version 1.1
+ */
  public class Hora
  {
     private byte Horas;
     private byte Minutos;
     private byte Segundos;
-
-
     
+    /**
+    * Constructor para objetos de la clase hora
+    */
     public Hora()
     {
-     //crea y/o inicializa el estado de un objeto
      Horas=0;
      Minutos=0;
      Segundos=0;
     }
    
-    //Constructor
+    /**
+     * Constructor para objetos de la clase hora recibiendo horas,
+     * minutos y segundos
+     */
     public Hora (byte _Horas, byte _Minutos, byte _Segundos)
     {
        setHora (_Horas);
@@ -23,6 +32,10 @@
        setSec (_Segundos);
     }
     
+    /**
+    * Constructor para objetos de la clase hora recibiendo horas 
+    * y minutos
+    */
     public Hora (byte _Horas, byte _Minutos)
     {
        setHora (_Horas);
@@ -30,47 +43,53 @@
        Segundos = 0;
     }
     
-     public Hora (byte _Horas)
+    /**
+    * Constructor para objetos de la clase hora recibiendo horas
+    */
+    public Hora (byte _Horas)
     {
        setHora (_Horas);
        Minutos=0;
        Segundos=0;
     }
        
-    //Nueva hora entre 0 y 23
+    /**Nueva hora, siempre entre 0 y 23*/
     public void setHora (byte _Horas)
     {
     Horas = (_Horas <=23 && _Horas >= 0) ?_Horas:0;
     
     } 
     
+    /**Nuevos minutos siempre entre 0 y 59*/
     public void setMinutos(byte _Minutos){
         Minutos = _Minutos;
     }
     
+    /**Nuevos segundos siempre entre 0 y 59*/
     public void setSec (byte _Segundos)
     {
      Segundos = ( _Segundos <= 59 && _Segundos >= 0) ?_Segundos :0;
     }
 
+    /**Obtener hora actual*/
     public byte getHoras()
     {
      return Horas;
     }
     
-    
+    /**Obtener minuto actual*/
     public byte getMinutos()
     {
         return Minutos;
     }
 
-    
+    /**Obtener segundo actual*/
     public byte getSegundos()
     {
         return Segundos;
     }
     
-    
+    /**Obtener hora con formato*/
     public String toString( )
     {
         return ( (Horas == 12 || Horas == 0) ? 12 : Horas % 12) 
